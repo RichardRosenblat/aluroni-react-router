d412ef299e436e63e491dcafd44922de460d8abc
richard rosenblat <rosenblatr@gmail.com>
2022-09-17 11:34:14 -0300

    estilo tema

--- src/components/PaginaPadrao/index.tsx
+++ src/components/PaginaPadrao/index.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header";
+import stylesTema from "styles/Tema.module.scss";
+
 
 export default function PaginaPadrao() {
 
     return (             
         <>
             <Header/>
-            <div>
+            <div className={stylesTema.container}>
                 <Outlet/>
             </div>
         </>
--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,11 +1,12 @@
 import cardapio from "data/cardapio.json";
 import styles from "./Inicio.module.scss";
+import stylesTema from "styles/Tema.module.scss";
 
 export default function Inicio() {
     let pratosRecomendados = [...cardapio];
     pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).splice(0,3);
     return (
-        <section className={styles.titulo}>
+        <section className={stylesTema.titulo}>
             <h3>Recomendações da cozinha</h3>
             <div className={styles.recomendados }>
                 {pratosRecomendados.map(item=> (
