0508385979d4b97ce02ada6fcdf6dca7d4f8b46e
richard rosenblat <rosenblatr@gmail.com>
2022-09-17 12:15:23 -0300

    fix key, finish inico and sobre

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -5,24 +5,29 @@ import { Link } from "react-router-dom";
 export default function Menu() {
     const rotas = [
         {
+            id:"57a449be-eb58-4dcd-9a80-da0e519098f8",
             label:"Início",
-            to:"/"
+            to:"/",
         },
         {
+            id:"98b02f8d-5725-474c-8807-ca5b351cf462",
             label:"Cardápio",
-            to:"/cardapio"
+            to:"/cardapio",
         },
         {
+            id:"4016ea33-f352-46c1-9542-2816a80a951f",
             label:"Sobre",
-            to:"/sobre"
+            to:"/sobre",
         }
     ];
     return (             
         <nav className={styles.menu}>
-            <Logo />
+            <Link to="/">
+                <Logo />
+            </Link>
             <ul className={styles.menu__list}>
-                {rotas.map((rota,index)=>(
-                    <li key={index} className={styles.menu__link}>
+                {rotas.map((rota)=>(
+                    <li key={rota.id} className={styles.menu__link}>
                         <Link to={rota.to}>{rota.label}</Link>
                     </li>
                 ))}
